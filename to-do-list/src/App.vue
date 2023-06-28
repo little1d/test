1d7fa3d7eaa530bd43373245c180b2b33d03779f
yang_zhuo <2431542525@qq.com>
2023-06-28 22:46:11 +0800

    增加了一些样式
    使用tailwindcss更改了一些样式

--- to-do-list/src/App.vue
+++ to-do-list/src/App.vue
@@ -58,20 +58,22 @@ import {
 </script>
 
 <template>
-  <div class="common-layout flex flex-col container mx-auto relative w-90 h-70 gap-8">
-    <div class="">Welcome to your to-do-list!</div>
-    <el-input v-model="name" placeholder="name here!" class="" size="large" />
-    <el-container>
+  <div class="py-5" style="background-image: url(./q.jpg);">
+    <div class="bg-gradient-to-r from-purple-500 to-pink-500 h-screen px-20 mx-20 saturate-50 backdrop-blur-sm">
+    <div class="common-layout flex flex-col container px-80 gap-5 font-bold font-black">
+    <div class="py-5">Welcome to your to-do-list!
+      <el-input v-model="name" placeholder="name here!" class="w-50 py-4" size="large" />
+    </div>
+
       <el-header>
         <div>Create-todo</div>
-        <el-input v-model="input_content" placeholder="e.g. make a video" class="w-50 m-2" size="large" />
-
+        <el-input v-model="input_content" placeholder="e.g. make a video" class="w-50 m-2 py-4" size="large" />
       </el-header>
 
       <el-main>
-        <div>Pick a category</div>
-        <el-radio-group v-model="input_category" size="large">
-          <el-radio-button label="School" value="School" />
+        <div class="py-2">Pick a category</div>
+        <el-radio-group v-model="input_category" size="large" class="flex gap-4 py-4">
+          <el-radio-button label="School" value="School" class=""/>
           <el-radio-button label="Home" value="Home" />
           <el-radio-button label="Club" value="Club" />
           <el-radio-button label="Friends" value="Friends" />
@@ -85,21 +87,23 @@ import {
         <div class="flex flex-col space-y-5">
           <!-- 创建to-do-list -->
           <div v-for="todo in todos_asc">
-            <div class="todo-content flex flex-row">
-              <input type="checkbox" v-model="todo.done" class="flex-none">
-              <span v-if="todo.done" style="text-decoration: line-through" class="flex-1">{{ todo.content }}</span>
-              <span v-else class="flex-1">{{ todo.content }}</span>
-              <el-button type="danger" :icon="Delete" circle @click="removeTodo(todo)" class="flex-none"></el-button>
+            <div class="flex justify-around">
+              <el-checkbox-button type="checkbox" v-model="todo.done" size="large">
+                <span v-if="todo.done" style="text-decoration: line-through" class="flex-1">{{ todo.content }}</span>
+                <span v-else class="flex-1">{{ todo.content }}</span>
+              </el-checkbox-button>
+              <el-button type="danger" :icon="Delete" circle @click="removeTodo(todo)" class=""></el-button>
             </div>
           </div>
         </div>
-        <div class="content-center	">
+        <div class="flex justify-center">
           <el-button type="danger" :icon="Delete" @click="clearAll" class="center w-36 hover:w-40">clear all</el-button>
         </div>
-
       </el-footer>
-    </el-container>
+    </div>
   </div>
+  </div>
+  
 </template>
 
 
